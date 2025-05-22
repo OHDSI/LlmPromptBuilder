c326fcb0eccb5dc801b7b57e3435d5f696db0af6
Gowtham Rao <rao@ohdsi.org>
2025-05-22 06:21:02 -0400

    Add root level is_relevant helper

---
+++ is_relevant.py
@@ -0,0 +1,9 @@
+from llm_prompt_builders.prompts.is_relevant import (
+    DEFAULT_POSITIVE_CRITERIA_IS_RELEVANT,
+    get_is_relevant,
+)
+
+__all__ = [
+    "DEFAULT_POSITIVE_CRITERIA_IS_RELEVANT",
+    "get_is_relevant",
+]
