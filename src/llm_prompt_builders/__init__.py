ce6f6a773072525ae88e437bbb7060cdc726c7b6
Gowtham Rao <rao@ohdsi.org>
2025-07-04 09:04:27 -0400

    Release v0.3.0
    Add cohort logic

--- src/llm_prompt_builders/__init__.py
+++ src/llm_prompt_builders/__init__.py
@@ -1,7 +1,7 @@
 """OHDSI llm_prompt_builders package root."""
 
 # ---- static string so Hatchling can read the version -----------------------
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 # ---------------------------------------------------------------------------
 
 from importlib.metadata import version, PackageNotFoundError
